3c6bd2214ff2c8120c648c0316d720fa5d07ff1b
ishika vishwakarma <132936647+ishikavishwakarma@users.noreply.github.com>
2024-09-27 14:05:46 +0530

    complete both question of js

--- program1.js
+++ program1.js
@@ -3,9 +3,28 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    let arr = [];
+    const mapping = {
+        ')':'(',
+        '}':'{',
+        ']':'[',
+    }
+    for(let i =0;i<s.length;i++){
+        let char=s[i];
+        if (char in mapping) {
+            let topElem=arr.length ? arr.pop():'#';
+            if(topElem!==mapping[char]){
+                return false;
+            }
+        }else{
+            arr.push(char)
+        }
+    }
+    return arr.length===0;
 };
 
 module.exports = { isValid };
+console.log(isValid("()"))
+console.log(isValid("("))
 
 
--- program2.js
+++ program2.js
@@ -3,8 +3,29 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    const romanNum={
+        "I":1,
+        "V":5,
+        "X":10,
+        "L":50,
+        "C":100,
+        "D":500,
+        "M":1000
+    }
+    let totalSum = 0;
+    for(let i =0;i<s.length;i++){
+        let currentValue = romanNum[s[i]];
+        let nextValue = romanNum[s[i+1]];
+        if(nextValue && currentValue<nextValue){
+            totalSum-=currentValue
+        }else{
+            totalSum+=currentValue;
+        }
+    }
+    return totalSum;
 };
 
 
 module.exports={romanToInt}
+console.log(romanToInt("III"))
+console.log(romanToInt("XL"))
